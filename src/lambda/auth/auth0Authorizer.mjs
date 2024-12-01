f9d4245cfb7d5dd50e16b6ca2cc70ca19ec54007
Dylan Vu <duyvd0702@gmail.com>
2024-12-02 03:19:23 +0700

    done auth function

---
+++ src/lambda/auth/auth0Authorizer.mjs
@@ -0,0 +1,78 @@
+import Axios from 'axios'
+import jsonwebtoken from 'jsonwebtoken'
+import { createLogger } from '../../utils/logger.mjs'
+
+const logger = createLogger('auth')
+
+const jwksUrl =
+  'https://dev-jinasf087yc373pq.us.auth0.com/.well-known/jwks.json'
+
+export async function handler(event) {
+  try {
+    const jwtToken = await verifyToken(event.authorizationToken)
+
+    return {
+      principalId: jwtToken.sub,
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Allow',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  } catch (e) {
+    logger.error('User not authorized', { error: e.message })
+
+    return {
+      principalId: 'user',
+      policyDocument: {
+        Version: '2012-10-17',
+        Statement: [
+          {
+            Action: 'execute-api:Invoke',
+            Effect: 'Deny',
+            Resource: '*'
+          }
+        ]
+      }
+    }
+  }
+}
+
+async function verifyToken(authHeader) {
+  const token = getToken(authHeader)
+  const jwt = jsonwebtoken.decode(token, { complete: true })
+
+  // TODO: Implement token verification
+  const res = await Axios.get(jwksUrl)
+  const keys = res.data.keys
+  const signingKeys = keys?.find((key) => key.kid === jwt.header.kid)
+
+  if (!signingKeys) {
+    throw new Error('No key found in JWKS endpoint')
+  }
+
+  const pem = signingKeys?.x5c[0]
+
+  const certificate = `=====Start Certificate=====\n${pem}\n=====End Certificate=====`
+
+  const verifiedToken = verify(token, certificate, { algorithms: ['RS256'] })
+
+  return verifiedToken
+}
+
+function getToken(authHeader) {
+  if (!authHeader) throw new Error('No authentication header')
+
+  if (!authHeader.toLowerCase().startsWith('bearer '))
+    throw new Error('Invalid authentication header')
+
+  const split = authHeader.split(' ')
+  const token = split[1]
+
+  return token
+}
